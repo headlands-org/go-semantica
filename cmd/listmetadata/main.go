2426a0d61b015b80d05981a81cf8f929efca7ce9
Lloyd Hilaiel <lloyd@hilaiel.com>
2025-10-30 02:00:45 -0600

    Add helper CLIs for inspecting GGUF models

---
+++ cmd/listmetadata/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"reflect"
+	"unsafe"
+
+	"github.com/lth/pure-go-llamas/internal/gguf"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		log.Fatalf("usage: %s <model.gguf>", os.Args[0])
+	}
+
+	reader, err := gguf.Open(os.Args[1])
+	if err != nil {
+		log.Fatalf("open gguf: %v", err)
+	}
+	defer reader.Close()
+
+	rv := reflect.ValueOf(reader).Elem().FieldByName("metadata")
+	metadata := reflect.NewAt(rv.Type(), unsafe.Pointer(rv.UnsafeAddr())).Elem()
+	iter := metadata.MapRange()
+	for iter.Next() {
+		key := iter.Key().String()
+		val := iter.Value().Interface().(gguf.Metadata).Value
+		fmt.Printf("%s: %v\n", key, val)
+	}
+}
